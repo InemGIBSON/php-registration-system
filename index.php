20841c34f4c0277727801c35fe35cc90dac48ba2
Inemesit Gibson <inemgibs@gmail.com>
2026-03-17 20:09:22 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h2>User Registration</h2>
+    <form method="POST" action="register.php">
+        Name: <input type="text" name="name" required><br><br>
+        Email: <input type="email" name="email" required><br><br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
